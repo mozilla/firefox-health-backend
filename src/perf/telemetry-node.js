37f79e5de5d646f4295b890b7ea354553e0722ac
Sarah Clements <sclements313@gmail.com>
2018-04-11 18:12:17 -0700

    Fix getEvolution bugs

--- src/perf/telemetry-node.js
+++ src/perf/telemetry-node.js
@@ -514,18 +514,25 @@ Telemetry.getEvolution = function Telemetry_getEvolution(channel, version,
       if (!Array.isArray(filter)) {
         filter = [filter];
       }
-
       for (let i = 0; i < filter.length; ++i) {
         filterString += `&${encodeURIComponent(filterName)}=${
           encodeURIComponent(filter[i])}`;
       }
     });
+
+    if (metric === 'TIME_TO_NON_BLANK_PAINT_NO_NETOPT_MS' && version < '59') {
+      metric = 'TIME_TO_NON_BLANK_PAINT_MS';
+    } else if (metric === 'TIME_TO_NON_BLANK_PAINT_MS' && version >= '59') {
+      metric = 'TIME_TO_NON_BLANK_PAINT_NO_NETOPT_MS';
+    }
+    console.log(channel, version, metric);
   const url = `${Telemetry.BASE_URL}aggregates_by/${useSubmissionDate ?
       'submission_date' : 'build_id'
     }/channels/${encodeURIComponent(channel)}/?version=${
     encodeURIComponent(version)}&dates=${
     encodeURIComponent(dates)}&metric=${encodeURIComponent(metric)
     }${filterString}`;
+  // console.log(url);
   const entriesMap = {};
   if (url.length > Telemetry.MAX_URL_LENGTH) {
     assert(useSubmissionDate, '`url` is too long because of build_id?!');
