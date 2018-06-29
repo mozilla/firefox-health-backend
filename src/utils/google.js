0c955d52e1bbb2eb2ccbc26f5de5f65d6d28c122
Armen Zambrano G <armenzg@gmail.com>
2018-06-29 12:12:10 -0400

    Throw 400 error if server started without GOOGLE_API_KEY for /api/android/klar

--- src/utils/google.js
+++ src/utils/google.js
@@ -4,6 +4,10 @@ import fetchJson from '../fetch/json';
 export const getSpreadsheetValues = async ({ id, range }) => {
   const sheetsAPI = 'https://sheets.googleapis.com/v4/spreadsheets';
   const authKey = process.env.GOOGLE_API_KEY;
+  if (!authKey) {
+    console.error('The server was started without setting the GOOGLE_API_KEY');
+    return [];
+  }
 
   const url = `${sheetsAPI}/${id}/values/${range}?key=${authKey}`;
   const { values } = await fetchJson(url);
