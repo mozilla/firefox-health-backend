047aa2d80ceb0ba7e5e7dfb46535e1e6af88ca42
Armen Zambrano G <armenzg@gmail.com>
2019-03-12 09:50:31 -0400

    Issue #54 - Remove support for fetching data from Google spreadsheets
    This removes the /perf/notes and /android/klar apis

--- src/configuration.js
+++ src/configuration.js
@@ -1,6 +1,4 @@
 const config = {
-  quantumSpreadsheetId: '1UMsy_sZkdgtElr2buwRtABuyA3GY6wNK_pfF01c890A',
-  androidSpreadsheetId: '1vE0b3tawWY9vVNq9Ds6CiA9XZ6LStkcXZl3F8dwXid8',
   nimbledroidApiUrl: 'https://nimbledroid.com/api/v2/users/npark@mozilla.com/apps',
   repoUrl: 'https://github.com/mozilla/firefox-health-backend',
 };
